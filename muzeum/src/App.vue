c4395a3f3edc62995159ac58c3d5ad7b197b9539
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-01-18 16:09:52 +0100

    Új szobor létrehozása

--- muzeum/src/App.vue
+++ muzeum/src/App.vue
@@ -21,6 +21,15 @@
         <td>{{ statue.created_at }}</td>
         <td>{{ statue.updated_at }}</td>
       </tr>
+      <tr>
+        <td><input type="hidden" v-model="statue.id"></td>
+        <td><input type="text" v-model="statue.person"></td>
+        <td><input type="number" v-model="statue.height"></td>
+        <td><input type="number" v-model="statue.price"></td>
+        <td><button v-if="add_new" @click="saveStatue">Save</button></td>
+        <td><button v-if="add_new" @click="cancelStatue">Cancel</button></td>
+        <td><button @click="newStatue" :disabled="saving" v-if="!add_new">Add</button></td>
+      </tr>
     </tbody>
   </table>
 </div>
@@ -32,7 +41,15 @@ export default {
   name: 'App',
   data() {
     return {
-      statues: []
+      statues: [],
+      add_new: false,
+      saving: false,
+
+      statue: {
+        person: '',
+        height: null,
+        price: null
+      }
     }
   },
   methods: {
@@ -40,6 +57,41 @@ export default {
       let response = await fetch("http://127.0.0.1:8000/api/statues")
       //let data = await response.json()
       this.statues = await response.json()
+    },
+
+    async newStatue() {
+      this.saving = 'disabled'
+      let response = await fetch('http://127.0.0.1:8000/api/statues', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json',
+          'Accept' : 'application/json'
+        },
+        body: JSON.stringify(this.statue)
+      })
+      console.log(response)
+      await this.loadData()
+      this.saving = false
+      this.resetForm()
+    },
+
+    async saveStatue() {
+      this.saving = 'disabled'
+      await fetch(`http://127.0.0.1:8000/api/paintings/${this.statue.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type' : 'application/json',
+          'Accept' : 'application/json'
+        },
+        body: JSON.stringify(this.statue)
+      })
+      await this.loadData()
+      this.saving = false
+      this.resetForm()
+    },
+
+    cancelStatue() {
+      this.resetForm()
     }
   },
   mounted() {
