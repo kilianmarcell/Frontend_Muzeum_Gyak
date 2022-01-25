ab1ebe080e180995752ff71d9669d0a0a187fd30
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-01-25 16:13:15 +0100

    TODO megjelenítés

--- muzeum/src/main.js
+++ muzeum/src/main.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 
 import Paintings from './components/Paintings.vue';
 import Statues from './components/Statues.vue';
+import Statue from './components/Statue.vue';
 import Info from './components/Info.vue';
 
 Vue.use(VueRouter) //ez a plugin
@@ -14,6 +15,10 @@ const routes = [
     {
         path: '/statues',
         components: Statues
+    },
+    {
+        path: '/statues/:id',
+        components: Statue
     },
     {
         path: '/paintings',
